7bbdb3c8d1023b7850cadcee6c6eb7f1cca382e1
Kameron Bielawski <kam@ip0af5c5fa.int.uvm.edu>
2024-01-20 18:44:25 -0500

    loop timesteps

--- simulation.py
+++ simulation.py
@@ -1,4 +1,15 @@
+import time
+
 import pybullet as p
 
+# Create physics sim client
 physics_client = p.connect(p.GUI)
+
+# Run simulation for 1000 steps
+n_timesteps = 1000
+for i in range(n_timesteps):
+    p.stepSimulation()
+    time.sleep(0.1)
+
+# Disconnect from physics client
 p.disconnect()

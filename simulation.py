f9d26500ce95c226bb68e9c33ff0393be413bb0c
Kameron Bielawski <kam@ip0af5c5fa.int.uvm.edu>
2024-01-20 18:53:22 -0500

    one link

--- simulation.py
+++ simulation.py
@@ -5,11 +5,14 @@ import pybullet as p
 # Create physics sim client
 physics_client = p.connect(p.GUI)
 
+# Bring link in
+p.loadSDF('box.sdf')
+
 # Run simulation for 1000 steps
 n_timesteps = 1000
 for i in range(n_timesteps):
     p.stepSimulation()
-    time.sleep(0.1)
+    time.sleep(0.01)
 
 # Disconnect from physics client
 p.disconnect()

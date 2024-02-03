a705804a636640fa215db60fae7b64712d737bed
Kameron Bielawski <kam@EMCFs-MacBook-Pro-4.local>
2024-02-03 11:40:39 -0500

    locomotor

--- simulate.py
+++ simulate.py
@@ -25,13 +25,44 @@ pyrosim.Prepare_To_Simulate(robot_id)
 n_timesteps = 1000
 backleg_sensor_values = np.zeros(n_timesteps)
 frontleg_sensor_values = np.zeros(n_timesteps)
+
+t = np.arange(n_timesteps)
+amplitude_back = np.pi / 4
+amplitude_front = np.pi / 4
+
+freq_back = np.pi / 50
+freq_front = np.pi / 55
+phase_offset_back = 0
+phase_offset_front = np.pi / 4
+
+backleg_motor_values = amplitude_back * np.sin(freq_back * t + phase_offset_back)
+frontleg_motor_values = amplitude_front * np.sin(freq_front * t + phase_offset_front)
+
 for i in range(n_timesteps):
     p.stepSimulation()
     back_leg_sensor = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     front_leg_sensor = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     backleg_sensor_values[i] = back_leg_sensor
     frontleg_sensor_values[i] = front_leg_sensor
-    time.sleep(0.001)
+
+    back_motor = np.pi/2*(np.random.random() * 2 - 1)
+    front_motor = np.pi/2*(np.random.random() * 2 - 1)
+
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex = robot_id,
+        jointName = "Torso_BackLeg",
+        controlMode = p.POSITION_CONTROL,
+        targetPosition = backleg_motor_values[i],
+        maxForce = 50)
+
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex = robot_id,
+        jointName = "Torso_FrontLeg",
+        controlMode = p.POSITION_CONTROL,
+        targetPosition = frontleg_motor_values[i],
+        maxForce = 50)
+
+    time.sleep(0.0001)
 
 np.save('./data/sensor_vals.npy', np.array([backleg_sensor_values, frontleg_sensor_values]))
 
